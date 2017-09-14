d894019cf91fa0a4aa817eb064d71a0451337488
sigurlaug <sigurlaugthordar@gmail.com>
2017-09-14 16:06:03 +0000

    Controllers

---
+++ src/main/java/main/controller/AccountController.java
@@ -0,0 +1,22 @@
+package main.controller;
+
+
+/**
+ * @author Sigurlaug Þórðardóttir
+ * @date September 2017
+ * Háskóli Íslands
+ *
+ * Stýriklasi fyrir aðgang notenda að kerfinu
+ */
+public class AccountController {
+
+    //Þetta fall bætir nýjum notanda í kerfið
+    public void addUser(){
+
+    }
+
+    //Þetta fall sér um að skrá inn notanda
+    public boolean login(){
+        return true;
+    }
+}
