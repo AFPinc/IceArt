4099a08856547d043024c0ec16c1727bd40f09f9
valgerdur <vas30@hi.is>
2017-10-31 12:11:03 +0000

    SmokeTest

--- src/test/java/main/SmokeTest.java
+++ src/test/java/main/SmokeTest.java
@@ -1,19 +1,22 @@
 package main;
 
-import main.controller.EventController;
+import main.controller.*;
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 /**
- *
- * @author Ebba Þóra Hvannberg
+ * @author Ása Júlía Aðalsteinsdóttir
+ * @author Melkorka Mjöll Jóhansdóttir
+ * @author Sigurlaug Þórðardóttir
+ * @author Vagerður Sigfinnsdóttir
  * @date október 2017 
  * HBV501G Hugbúnaðarverkefni 1 Háskóli Íslands
  * 
- * Prófunarklasi sem athugar hvort KennariController keyrir 
+ * Prófunarklasi sem athugar hvort Controller klasarnir keyri
 */
 
 @RunWith(SpringRunner.class)
@@ -21,15 +24,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SmokeTest {
 
         @Autowired
+        ArtistController artistController;
+
+        @Autowired
         EventController eventController;
+
+        @Autowired
+        LocationController locationController;
+
+        @Autowired
+        MainController mainController;
+
+        @Autowired
+        SearchController searchController;
         /**
          * Aðferð til að athuga hvort KennariController hlutur hefur verið búinn til 
          */
         
 	@Test
 	public void contextLoads() {
-        
-                   assertThat(eventController).isNotNull();
+
+                    assertThat(artistController).isNotNull();
+                    assertThat(eventController).isNotNull();
+                    assertThat(locationController).isNotNull();
+                    assertThat(mainController).isNotNull();
+                    assertThat(searchController).isNotNull();
 	}
 
 }
